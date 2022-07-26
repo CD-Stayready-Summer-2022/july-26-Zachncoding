0711355627946c8ba4ebfb5af558765dd174fd61
Cameron WIlson <cawil29@morgan.edu>
2022-07-26 09:34:32 -0400

    Failed

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,19 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public String howManyLettersDoYouSee(String input){
+    public String howManyLettersDoYouSee(String input) {
+        String str = " ";
+        Map<String, Integer> hashMap = new HashMap<>();
+        String[] letters = str.split(" ");
+
+        for (String letter : letters)
+            if (hashMap.containsKey(letter))
+                hashMap.put(letter, hashMap.get(letter) + 1);
+            else
+                hashMap.put(letter, 1);
         return null;
     }
 }
